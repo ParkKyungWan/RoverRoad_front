b2feeb713a10a15c7cef8f5c15a297ed904b5fe6
ParkKyungWan <pkw8117197@gmail.com>
2022-08-10 20:27:09 +0900

    im_so_tired_no_more_coding_today

--- src/pages/calander/Calander.js
+++ src/pages/calander/Calander.js
@@ -3,13 +3,20 @@ import React from 'react';
 //scss
 import './Calander.scss';
 
+//image
+import newButton from 'images/calander/newButton.svg';
 
 export default function Calander(){
 
     return(
         <div>
             <div className='calander entire'>
-                <h1>hi</h1>
+                <div className='title-box'>
+                <img src={newButton} className="new-button push" />
+                    <h1>일정 만들기</h1>
+                </div>
+                <h2>직접 만든 일정</h2>
+                <h2>자동으로 기록한 여행</h2>
             </div>
         </div>
     )
